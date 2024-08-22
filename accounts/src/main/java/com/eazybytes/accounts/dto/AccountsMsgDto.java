c4e74fbf045ff0853188e106acd0929d1ee1c25f
Kartik Vedalaveni <kartik.vedalaveni@verisk.com>
2024-08-22 15:56:52 +0530

    initial-commit.

---
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsMsgDto.java
@@ -0,0 +1,12 @@
+package com.eazybytes.accounts.dto;
+
+/**
+ * @param accountNumber
+ * @param name
+ * @param email
+ * @param mobileNumber
+ */
+public record AccountsMsgDto(
+        Long accountNumber, String name, String email, String mobileNumber
+) {
+}
